28c27a595038e80655d880c02480614249ebf8ef
Satoshi Nagasak <satoshi.nagasaka21@gmail.com>
2016-06-30 02:48:58 +0900

    first commit

---
+++ Annicted/AuthViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AuthViewController.swift
+//  Annicted
+//
+//  Created by 長坂悟志 on 2016/06/30.
+//  Copyright © 2016年 SatoshiN21. All rights reserved.
+//
+
+import UIKit
+
+class AuthViewController: UIViewController,UIWebViewDelegate {
+
+    @IBOutlet weak var webView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let param = ["client_id":AnnictApi.ClientId,"response_type":"code","redirect_uri":"https://jp.hatenadiary.satoshin21/redirect_uri","scope":"read+write"]
+        
+        
+//        let url = AnnictApi.BaseUrl + AnnictApi.OAuthAuthorize
+        let url = "\(AnnictApi.BaseUrl + AnnictApi.OAuthAuthorize)?\(param.map({"\($0)=\($1)"}).joinWithSeparator("&"))"
+        
+        webView.loadRequest(NSURLRequest(URL: NSURL(string: url)!))
+    }
+    
+    func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        if let url = request.URL?.absoluteString {
+            print("URL:\(url)")
+        }
+        return true
+    }
+}
---
+++ Annicted/Const.swift
@@ -0,0 +1,18 @@
+//
+//  Const.swift
+//  Annicted
+//
+//  Created by 長坂悟志 on 2016/06/30.
+//  Copyright © 2016年 SatoshiN21. All rights reserved.
+//
+
+import UIKit
+
+struct AnnictApi {
+    
+    static var ClientId = "5e0890ed674ec454caca71c0d8e530ba6f74edfff033a32fce439a4441bd1884"
+    
+    static var BaseUrl = "https://api.annict.com"
+    
+    static var OAuthAuthorize = "/oauth/authorize"
+}
