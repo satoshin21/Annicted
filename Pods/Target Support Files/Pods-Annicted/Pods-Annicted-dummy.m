aebeb1ddc006986cbdeca869e3b7881e1a75f623
Satoshi Nagasak <satoshi.nagasaka21@gmail.com>
2016-07-01 02:17:32 +0900

    add alamofire library

---
+++ Pods/Target Support Files/Pods-Annicted/Pods-Annicted-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Annicted : NSObject
+@end
+@implementation PodsDummy_Pods_Annicted
+@end
---
+++ Pods/Target Support Files/Pods-Annicted/Pods-Annicted-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_AnnictedVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_AnnictedVersionString[];
+
